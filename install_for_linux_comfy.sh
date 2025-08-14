3081f70ba00caa7ac899fecc3c05f1fa6c54ec16
MacGuyver-DEV <89907172+macguyversmusic@users.noreply.github.com>
2025-08-14 17:16:06 -0500

    Update install_for_linux_comfy.sh

--- install_for_linux_comfy.sh
+++ install_for_linux_comfy.sh
@@ -7,7 +7,7 @@ echo "Installing requirements..."
 python -m pip install -r "$requirements_txt"
 
 echo "Fixing dependencies..."
-python -m pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless
-python -m pip install -r "$repair_dependency_txt"
+python3 -m pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless opencv-contrib-python-headless
+python3 -m pip install -r "$repair_dependency_txt"
 
 echo "Install Finish!"
